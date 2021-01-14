62b9ac5673eb3d4557bde25d9f4c4c4d8f5caba5
Victoria Landron <viclandron@gmail.com>
2021-01-13 20:55:54 -0500

    adding authetication

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
