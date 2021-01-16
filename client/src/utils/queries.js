c8f8858c46133b1d256a3bff8c19bbc606e89626
Victoria Landron <viclandron@gmail.com>
2021-01-16 16:31:22 -0500

    connecting all the pages

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,3 +17,24 @@ export const QUERY_THOUGHTS = gql`
     }
   }
 `;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
